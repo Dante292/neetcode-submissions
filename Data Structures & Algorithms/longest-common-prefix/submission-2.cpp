26627048e596f45846ec7282e66848e7ac1d7c54
Dante292 <paragbobo@gmail.com>
2026-09-17 09:46:44 +0530

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        sort(strs.begin(), strs.end());
+        string a ="";
+        for(int i = 0; i < strs[0].size(); i++){
+            if(strs[0][i] == strs.back()[i]){
+                a = a + strs[0][i];
+            } else {
+                return a;
+            }
+        }
+        
+        return a;
+    }
+};
